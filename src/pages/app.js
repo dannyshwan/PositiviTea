513dd624e22d622fe640307ac5cf24171d8febf7
Ryan Ly <ryanly.rl1@gmail.com>
2020-02-01 14:38:34 -0500

    Add logo to page

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/app.scss';
 import main from '../assets/main-tea.png';
+import logo from '../assets/logo.png';
 
 export class App extends React.Component{
   render(){
@@ -8,7 +9,7 @@ export class App extends React.Component{
       <body>
         <div class='container'>
           <div id='content'>
-            <h1>POSITIVITEA</h1>
+            <h1><img src={logo} className="logo" alt="logo"/></h1>
             <img src={main} className="landing-img" alt="main-tea"/>
           </div>
         </div>
