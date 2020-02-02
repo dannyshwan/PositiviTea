2b18c53f0c1365cb325133ebe7a92aa4e1be6270
Ellen <eli030@uottawa.ca>
2020-02-02 09:29:58 -0500

    ui changes

--- src/views/Form.jsx
+++ src/views/Form.jsx
@@ -3,52 +3,89 @@ import { Link } from "react-router-dom";
 import '../styles/app.scss';
 import { Formik, Form as formikForm } from 'formik';
 import NavBar from '../NavBar.jsx'
+import {Button, TextField, Slider, Typography} from '@material-ui/core';
 
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'lol'
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div class='container'>
       <NavBar/>
+      <div className="form-container">
+        <div className="col2">
         <h1>Let's give this a Chai...</h1>
-        <Formik
-          initialValues={{
-            firstName: '', 
-            lastName: '',
-            feeling: ''
-          }}
-        >
-          {() => (
-          <formikForm>
-            <label htmlFor='name' style={{display: 'block'}}>What is your name?</label>
-            <input
-              id="name"
-              placeholder="Please enter your name"
-              type="text"
-              >
-            </input>
-            <label htmlFor='feeling' style={{display: 'block'}}>How are you feeling?</label>
-            <select
-              id="name"
-              placeholder="How are you feeling?"
-            >
-              <option value="excited">Excited</option>
-              <option value="stressed">Stressed</option>
-              <option value="emotional">Emotional</option>
-              <option value="an">Audi</option>
-            </select>
-            <label htmlFor='color' style={{display: 'block'}}>What's your favorite color?</label>
-            <input 
-              type='color'
-              placeholder="#000000"
-            >
-            </input>
-            <Link to="/Results">
-              <button type='submit'> Next </button>
-            </Link>
-          </formikForm>
-          )}
-        </Formik>
+
+        <form noValidate autoComplete="off">
+          <TextField id="standard-basic" label="Name" value={this.state.value} onChange={this.handleChange}/>
+        </form>
+        <h3 style={{'font-weight': '400'}}>How happy are you feeling?</h3>
+        <Slider
+          className="slider"
+          aria-label="How happy are you feeling"
+          color="primary"
+          defaultValue={5}
+          valueLabelDisplay="auto"
+          marks
+          min={0}
+          max={10}
+          style={{'width':'50%', 'display': 'block', 'margin': '1em auto'}}
+        />
+        <h3 style={{'font-weight': '400'}}>How sad are you feeling?</h3>
+        <Slider
+          className="slider"
+          aria-label="How happy are you feeling"
+          color="primary"
+          defaultValue={5}
+          valueLabelDisplay="auto"
+          marks
+          min={0}
+          max={10}
+          style={{'width':'50%', 'display': 'block', 'margin': '1em auto'}}
+        />
+        <h3 style={{'font-weight': '400'}}>How nervous are you feeling?</h3>
+        <Slider
+          className="slider"
+          aria-label="How happy are you feeling"
+          color="primary"
+          defaultValue={5}
+          valueLabelDisplay="auto"
+          marks
+          min={0}
+          max={10}
+          style={{'width':'50%', 'display': 'block', 'margin': '1em auto'}}
+        />
+        <h3 style={{'font-weight': '400'}}>How emotional are you feeling?</h3>
+        <Slider
+          className="slider"
+          aria-label="How happy are you feeling"
+          color="primary"
+          defaultValue={5}
+          valueLabelDisplay="auto"
+          marks
+          min={0}
+          max={10}
+          style={{'width':'50%', 'display': 'block', 'margin': '1em auto'}}
+        />
+              <Link to="/Results">
+                <Button variant="contained" color="primary" style={{'padding': '20px', 'margin-top': '2em', 'font-family': 'Kalam', 'font-size':'1em'}}>
+                  Click here to continue.
+                </Button>
+              </Link>
+        </div>
+        </div>
       </div>
     )
   }
--- src/views/Results.jsx
+++ src/views/Results.jsx
@@ -6,28 +6,29 @@ import tea2 from '../assets/tea2.png';
 import tea3 from '../assets/tea3.png';
 import tea4 from '../assets/tea4.png';
 import NavBar from '../NavBar';
+import {Button} from '@material-ui/core';
 
 const ENDPOINT = "https://type.fit/api/quotes";
 const TEAS = [
   {
     name: "Tea 1",
     image: tea1,
-    fact: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula ultrices tincidunt. Cras cursus nulla suscipit erat porta, sed interdum nisi egestas. Etiam at mollis turpis, at volutpat nulla. Vestibulum varius ac massa vitae sollicitudin. Nam vulputate congue commodo. Vivamus bibendum justo nec orci sollicitudin vehicula. Morbi et commodo velit. Nunc iaculis metus pellentesque, finibus nisi at, iaculis nisl. Quisque ac vestibulum metus, at bibendum augue. Nulla lacinia convallis magna id tempus. Nulla ac eros mi. Quisque mi felis, feugiat sit amet mi eu, euismod consequat mi. "
+    fact: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula ultrices tincidunt. Cras cursus nulla suscipit erat porta, sed interdum nisi egestas. Etiam at mollis turpis, at volutpat nulla. Vestibulum varius ac massa vitae sollicitudin. Nam vulputate congue commodo. Vivamus bibendum justo nec orci sollicitudin vehicula. Morbi et commodo velit. Nunc iaculis metus pellentesque. "
   },
   {
     name: "Tea 2",
     image: tea2,
-    fact: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula ultrices tincidunt. Cras cursus nulla suscipit erat porta, sed interdum nisi egestas. Etiam at mollis turpis, at volutpat nulla. Vestibulum varius ac massa vitae sollicitudin. Nam vulputate congue commodo. Vivamus bibendum justo nec orci sollicitudin vehicula. Morbi et commodo velit. Nunc iaculis metus pellentesque, finibus nisi at, iaculis nisl. Quisque ac vestibulum metus, at bibendum augue. Nulla lacinia convallis magna id tempus. Nulla ac eros mi. Quisque mi felis, feugiat sit amet mi eu, euismod consequat mi. "
+    fact: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula ultrices tincidunt. Cras cursus nulla suscipit erat porta, sed interdum nisi egestas. Etiam at mollis turpis, at volutpat nulla. Vestibulum varius ac massa vitae sollicitudin. Nam vulputate congue commodo. Vivamus bibendum justo nec orci sollicitudin vehicula. Morbi et commodo velit. Nunc iaculis metus pellentesque. "
   },
   {
     name: "Tea 3",
     image: tea3,
-    fact: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula ultrices tincidunt. Cras cursus nulla suscipit erat porta, sed interdum nisi egestas. Etiam at mollis turpis, at volutpat nulla. Vestibulum varius ac massa vitae sollicitudin. Nam vulputate congue commodo. Vivamus bibendum justo nec orci sollicitudin vehicula. Morbi et commodo velit. Nunc iaculis metus pellentesque, finibus nisi at, iaculis nisl. Quisque ac vestibulum metus, at bibendum augue. Nulla lacinia convallis magna id tempus. Nulla ac eros mi. Quisque mi felis, feugiat sit amet mi eu, euismod consequat mi. "
+    fact: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula ultrices tincidunt. Cras cursus nulla suscipit erat porta, sed interdum nisi egestas. Etiam at mollis turpis, at volutpat nulla. Vestibulum varius ac massa vitae sollicitudin. Nam vulputate congue commodo. Vivamus bibendum justo nec orci sollicitudin vehicula. Morbi et commodo velit. Nunc iaculis metus pellentesque. "
   },
   {
     name: "Tea 4",
     image: tea4,
-    fact: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula ultrices tincidunt. Cras cursus nulla suscipit erat porta, sed interdum nisi egestas. Etiam at mollis turpis, at volutpat nulla. Vestibulum varius ac massa vitae sollicitudin. Nam vulputate congue commodo. Vivamus bibendum justo nec orci sollicitudin vehicula. Morbi et commodo velit. Nunc iaculis metus pellentesque, finibus nisi at, iaculis nisl. Quisque ac vestibulum metus, at bibendum augue. Nulla lacinia convallis magna id tempus. Nulla ac eros mi. Quisque mi felis, feugiat sit amet mi eu, euismod consequat mi. "
+    fact: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula ultrices tincidunt. Cras cursus nulla suscipit erat porta, sed interdum nisi egestas. Etiam at mollis turpis, at volutpat nulla. Vestibulum varius ac massa vitae sollicitudin. Nam vulputate congue commodo. Vivamus bibendum justo nec orci sollicitudin vehicula. Morbi et commodo velit. Nunc iaculis metus pellentesque. "
   }
 ];
 
@@ -36,7 +37,8 @@ class Results extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quotes: []
+      quotes: [],
+      user:""
     };
   }
   
@@ -54,22 +56,29 @@ class Results extends React.Component {
     let random_tea = Math.floor(Math.random()*TEAS.length);
 
     return (
-        
+
       <div class='container'>
-        <NavBar></NavBar>
+        <NavBar/>
         <div id='content'>
-          <h2> Hello{this.props.name ? `, ${this.props.name}.` : `.`} Your tea is...</h2>
-          <h1>{TEAS[random_tea].name}</h1>
-          <div id="Logo" class="result">
-              <Link to="/" className="result-tea">
-                    <img src={TEAS[random_tea].image} alt="" style={{"width": '100%'}}/>
+          <div className="result-container">
+            <h3> Hello{this.state.user ? `, ${this.state.user}.` : `.`} Your tea is...</h3>
+            <h1>{TEAS[random_tea].name}</h1>
+            <div id="Logo" class="result">
+                <Link to="/" className="result-tea">
+                      <img src={TEAS[random_tea].image} alt="" style={{"width": '90%'}}/>
+                </Link>
+                <div class="tea-description"><p>{TEAS[random_tea].fact}</p></div>
+            </div>
+            <div>
+              <h3>
+                {quotes ? quotes[Math.floor(Math.random()*quotes.length)] : ""}
+              </h3>
+            </div>
+            <Link to="/">
+                <Button variant="contained" color="primary" style={{'padding': '15px', 'font-family': 'Kalam', 'font-size':'1em'}}>
+                  Return home.
+                </Button>
               </Link>
-              <div class="tea-description"><p>{TEAS[random_tea].fact}</p></div>
-          </div>
-          <div>
-            <h2>
-              {quotes ? quotes[Math.floor(Math.random()*quotes.length)] : ""}
-            </h2>
           </div>
         </div>
       </div>
