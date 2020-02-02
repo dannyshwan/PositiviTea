299aa63c4bbdd92a97af4158c453fbf17768c8bb
Daniel Shwan <dannyshwan@gmail.com>
2020-02-02 00:32:32 -0500

    redeploy

---
+++ docs/precache-manifest.94184a1333234bbc135fbd5d093f361c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e467825aada98f06f43d7f29edbdb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6241f170099aad274d31",
+    "url": "/static/css/main.35e021f8.chunk.css"
+  },
+  {
+    "revision": "a4c516a7bc7bba08efc8",
+    "url": "/static/js/2.1604a46d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.1604a46d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6241f170099aad274d31",
+    "url": "/static/js/main.0bad2fa6.chunk.js"
+  },
+  {
+    "revision": "8f023142fd19e389878b",
+    "url": "/static/js/runtime-main.02381cab.js"
+  },
+  {
+    "revision": "3e972b58b8e4d5697963e9b5e28ebadb",
+    "url": "/static/media/logo-straight.3e972b58.png"
+  },
+  {
+    "revision": "c6a741c6f4b77cf1d91ac3c47f136ad3",
+    "url": "/static/media/main-tea.c6a741c6.png"
+  },
+  {
+    "revision": "c6a741c6f4b77cf1d91ac3c47f136ad3",
+    "url": "/static/media/tea1.c6a741c6.png"
+  },
+  {
+    "revision": "c6a741c6f4b77cf1d91ac3c47f136ad3",
+    "url": "/static/media/tea2.c6a741c6.png"
+  },
+  {
+    "revision": "c6a741c6f4b77cf1d91ac3c47f136ad3",
+    "url": "/static/media/tea3.c6a741c6.png"
+  },
+  {
+    "revision": "c6a741c6f4b77cf1d91ac3c47f136ad3",
+    "url": "/static/media/tea4.c6a741c6.png"
+  }
+]);
