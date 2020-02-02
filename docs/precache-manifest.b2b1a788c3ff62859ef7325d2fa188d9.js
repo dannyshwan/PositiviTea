beec81fbae92734bb165762fbdf31285137f2da7
Daniel Shwan <dannyshwan@gmail.com>
2020-02-01 20:53:15 -0500

    deployment

---
+++ docs/precache-manifest.b2b1a788c3ff62859ef7325d2fa188d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4acad653dcd6dd690fdf30384c279497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "082cf6e3bb90f22832ca",
+    "url": "/static/css/main.1be8d127.chunk.css"
+  },
+  {
+    "revision": "35b6523ef094728b0056",
+    "url": "/static/js/2.e3593c66.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.e3593c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082cf6e3bb90f22832ca",
+    "url": "/static/js/main.3245831a.chunk.js"
+  },
+  {
+    "revision": "8f023142fd19e389878b",
+    "url": "/static/js/runtime-main.02381cab.js"
+  },
+  {
+    "revision": "3e972b58b8e4d5697963e9b5e28ebadb",
+    "url": "/static/media/logo-straight.3e972b58.png"
+  },
+  {
+    "revision": "c6a741c6f4b77cf1d91ac3c47f136ad3",
+    "url": "/static/media/main-tea.c6a741c6.png"
+  }
+]);
